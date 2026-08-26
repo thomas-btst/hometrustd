86e7c45db5e7cb945e6fc41fad15cdf21821763c
Thomas BATISTA <85026445+thomas-btst@users.noreply.github.com>
2026-08-26 15:56:26 +0000

    feat(idle): integrate idle inhibitor with network monitor

--- main.go
+++ main.go
@@ -17,25 +17,53 @@ func main() {
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer cancel()
 
-	conn, err := godbus.ConnectSystemBus()
+	systemConn, err := godbus.ConnectSystemBus()
 	if err != nil {
 		slog.Error("Failed to connect to system dbus", slog.Any("error", err))
 		os.Exit(1)
 	}
 	defer func() {
-		if err := conn.Close(); err != nil {
-			slog.Error("Failed to cleanly close dbus connection", slog.Any("error", err))
+		if err := systemConn.Close(); err != nil {
+			slog.Error("Failed to cleanly close system dbus connection", slog.Any("error", err))
 		}
 	}()
 
-	networkMonitor := NewNetworkMonitor(conn)
+	sessionConn, err := godbus.ConnectSessionBus()
+	if err != nil {
+		slog.Error("Failed to connect to session dbus", slog.Any("error", err))
+		os.Exit(1)
+	}
+	defer func() {
+		if err := sessionConn.Close(); err != nil {
+			slog.Error("Failed to cleanly close session dbus connection", slog.Any("error", err))
+		}
+	}()
+
+	networkMonitor := NewNetworkMonitor(systemConn)
+
 	wifiStates, err := networkMonitor.Watch(ctx)
 	if err != nil {
 		slog.Error("Failed to start network monitor watch", slog.Any("error", err))
 		os.Exit(1)
 	}
 
+	idleInhibitor := NewIdleInhibitor(sessionConn)
 	for state := range wifiStates {
 		fmt.Printf("Network state: %s\n", state)
+
+		if state.Connected {
+			if err := idleInhibitor.Inhibit(
+				fmt.Sprintf(
+					"Connected to Wi-Fi network %s (%s)",
+					state.SSID, state.BSSID,
+				),
+			); err != nil {
+				slog.Error("Failed to inhibit idle", slog.Any("error", err))
+			}
+		} else {
+			if err := idleInhibitor.Uninhibit(); err != nil {
+				slog.Error("Failed to uninhibit idle", slog.Any("error", err))
+			}
+		}
 	}
 }
